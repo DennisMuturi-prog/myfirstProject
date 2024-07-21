121b7a58afac83035f9d88f6ece7a0e072b3d55a
Dennis <deynohmuturi@gmail.com>
2024-07-21 21:16:32 +0300

    feat: Add MatIconModule to imports in AddToCartComponent

--- src/app/components/add-to-cart/add-to-cart.component.ts
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-add-to-cart',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,MatIconModule],
   templateUrl: './add-to-cart.component.html',
   styleUrl: './add-to-cart.component.css'
 })
