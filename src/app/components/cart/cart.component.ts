85389c9a42bef5630c48fe17803aeeda1a4dd778
Dennis <deynohmuturi@gmail.com>
2024-07-21 16:13:45 +0300

    Refactor CartComponent imports and add totalCost$ observable

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,18 +1,45 @@
 import { Component, inject } from '@angular/core';
-import {MatTableModule} from '@angular/material/table'
 import { ShoppingCartService } from '../../services/shopping-cart.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { CartProduct } from '../../Types/Types';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay ,map} from 'rxjs';
+import { CartItemComponent } from '../cart-item/cart-item.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [MatTableModule,AsyncPipe],
+  imports: [
+    AsyncPipe,
+    CartItemComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    MatTableModule,
+    CurrencyPipe,
+    MatDividerModule
+  ],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
 export class CartComponent {
-  shoppingCartService=inject(ShoppingCartService)
-  cartItems$:Observable<CartProduct[]>=this.shoppingCartService.cartDetails$
-  columnsToDisplay=['title','brand','price','quantity']
+  router = inject(Router);
+  shoppingCartService = inject(ShoppingCartService);
+  cartItems$: Observable<CartProduct[]> =this.shoppingCartService.cartWithQuantityChanging$.pipe(shareReplay(1));
+  totalCost$=this.cartItems$.pipe(
+    map((items)=>items.reduce((acc,curr)=>acc+(curr.price*curr.quantity),0))
+  )
+  goToHome() {
+    this.router.navigate(['home']);
+  }
+  columnsToDisplay = ['title', 'price', 'quantity', 'totalPrice'];
 }
