7c95c7ec6cb661f35bbb7e8657fcdc4f72c49a80
Dennis <deynohmuturi@gmail.com>
2024-07-27 13:20:59 +0300

    feat: Add addressMap constant for storing address data

---
+++ src/app/components/checkout/address.ts
@@ -0,0 +1,7 @@
+export const addressMap=new Map<string,string[]>(
+    [
+        ['Kiambu',['Ruiru','Juja','Thika']],
+        ['Nairobi',['Roysambu','Karen','Cbd']],
+        ['Nakuru',['Molo','Naivasha']]
+    ]
+)
