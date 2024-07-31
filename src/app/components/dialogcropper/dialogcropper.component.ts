9d6977c070f3623fc0e55854a77024a82f217240
Dennis <deynohmuturi@gmail.com>
2024-07-31 15:54:57 +0300

    refactor: Remove unused import in dialogcropper.component.ts

--- src/app/components/dialogcropper/dialogcropper.component.ts
+++ src/app/components/dialogcropper/dialogcropper.component.ts
@@ -2,7 +2,6 @@ import { Component ,inject, signal} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog'
 import {ImageCropperComponent,ImageCroppedEvent} from 'ngx-image-cropper'
-import { from } from 'rxjs';
 
 interface ImageData{
   image:File
