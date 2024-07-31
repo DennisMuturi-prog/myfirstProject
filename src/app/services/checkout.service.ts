6efa6f70592e5b79cc63e1c54ef170412b7f96d3
Dennis <deynohmuturi@gmail.com>
2024-07-31 15:55:20 +0300

    refactor: Update CheckoutService to handle M-Pesa payments and add dialog for payment confirmation

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,65 +1,93 @@
 import { inject, Injectable } from '@angular/core';
-import { addDoc, collection, doc, DocumentReference, setDoc } from '@angular/fire/firestore';
-import { combineLatestWith, EMPTY, from, Observable, Subject, switchMap,of, tap } from 'rxjs';
+import {
+  addDoc,
+  collection,
+  doc,
+  DocumentReference,
+  setDoc,
+} from '@angular/fire/firestore';
+import {
+  combineLatestWith,
+  map,
+  EMPTY,
+  from,
+  Observable,
+  Subject,
+  switchMap,
+  of,
+  tap,
+} from 'rxjs';
 import { AuthService } from './auth.service';
 import { ShoppingCartService } from './shopping-cart.service';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MpesaPaymentDialogComponent } from '../components/mpesa-payment-dialog/mpesa-payment-dialog.component';
 
-interface Order{
-  productId:string,
-  quantity:number
+interface Order {
+  productId: string;
+  quantity: number;
 }
-interface ShippingInfo{
-  county:string,
-  city:string,
-  street:string,
-  zip:string,
+interface ShippingInfo {
+  county: string;
+  city: string;
+  street: string;
+  zip: string;
 }
-interface OrderDetails{
-  addressInfo:ShippingInfo,
-  deliveryMethod:string,
-  modeOfPayment:string
+interface OrderDetails {
+  addressInfo: ShippingInfo;
+  deliveryMethod: string;
+  modeOfPayment: string;
 }
-interface StripeResponse{
-  url:string
+interface StripeResponse {
+  url: string;
 }
-interface StripeUrl{
-  url:string
+interface StripeUrl {
+  url: string;
 }
-
-
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckoutService {
+  readonly dialog = inject(MatDialog);
+  router = inject(Router);
   http = inject(HttpClient);
   authService = inject(AuthService);
   shoppingCartService = inject(ShoppingCartService);
   cart$ = this.shoppingCartService.cartWithQuantityChanging$;
+  totalPrice$=this.cart$.pipe(
+    map((cartItems)=>cartItems.reduce((acc,curr)=>acc+curr.quantity*curr.price,0))
+  )
   firestore = this.authService.firestore;
   currentUser$ = this.authService.currentUser$;
   orderGoods = new Subject<OrderDetails>();
   orderGoodsAction$ = this.orderGoods.asObservable();
   createOrder$ = this.currentUser$.pipe(
-    combineLatestWith(this.cart$, this.orderGoodsAction$),
-    switchMap(([user, cartGoods, orderDetails]) => {
+    combineLatestWith(this.cart$, this.orderGoodsAction$,this.totalPrice$),
+    switchMap(([user, cartGoods, orderDetails,totalPrice]) => {
       if (user) {
         let myOrder: Order[] = cartGoods.map((good) => ({
           productId: good.pid,
           quantity: good.quantity,
         }));
-        return this.addOrderItems(myOrder, user.userId,orderDetails.deliveryMethod).pipe(
-          tap(ref=>console.log(ref)),
-          switchMap((orderRef)=>{
-            if(orderDetails.modeOfPayment=='Credit card'){
-              return this.initiateStripePayment(orderRef.id)
-            }
-            else{
-              return of('no action')
+        return this.addOrderItems(
+          myOrder,
+          user.userId,
+          orderDetails.deliveryMethod
+        ).pipe(
+          tap((ref) => console.log(ref)),
+          switchMap((orderRef) => {
+            if (orderDetails.modeOfPayment == 'Credit card') {
+              return this.initiateStripePayment(orderRef.id);
+            } else {
+              this.openDialog(orderRef.id,totalPrice);
+              return of('no action');
             }
           }),
-          switchMap(()=>this.saveUsershippingInfo(orderDetails.addressInfo,user.userId))
+          switchMap(() =>
+            this.saveUsershippingInfo(orderDetails.addressInfo, user.userId)
+          )
         );
       } else {
         return EMPTY;
@@ -97,11 +125,22 @@ export class CheckoutService {
       )
     );
   }
-  initiateStripePayment(orderId:string) {
-     return this.http.post<StripeResponse>(
-       'http://localhost:3000/stripePayment',
-       {orderId}
-     ).pipe(tap((res:StripeUrl)=>window.location.href=res.url));
+  initiateStripePayment(orderId: string):Observable<StripeUrl> {
+    return this.http
+      .post<StripeResponse>('http://localhost:3000/stripePayment', { orderId })
+      .pipe(tap((res: StripeUrl) => (window.location.href = res.url)));
+  }
+  openDialog(orderId: string,totalPrice:number): void {
+    this.dialog.open(MpesaPaymentDialogComponent, {
+      data:{orderId,totalPrice},
+      width: '500px',
+      height:'400px'
+    });
+  }
+  initiateMpesaPayment(orderId:string,phoneNumber:string){
+    return this.http
+      .post('http://localhost:3000/mpesa', {orderId,phoneNumber })
+      .pipe(tap(() => this.router.navigate(['home'])));
 
   }
 
